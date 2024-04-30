7bbe908f38bc825dd2e0e33cb96d29f38a2d2d69
BuildTools <unconfigured@null.spigotmc.org>
2024-04-30 13:40:36 -0300

    Client initial setup, adding TailwinCSS and its first components.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 text-white text-start py-4">
+      <h1 className="text-xl font-mono font-semibold">Poke-App</h1>
+    </header>
+  );
+};
+
+export default Header;
